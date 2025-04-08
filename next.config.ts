fad6b621a7a70f5476494050ef8c0cba86ed3ca1
Drikus Roor <drikusroor@gmail.com>
2025-04-08 13:59:56 +0200

    chore: update dependencies for MDX support and installation

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,16 @@
 import createNextIntlPlugin from 'next-intl/plugin';
 import type { NextConfig } from "next";
+import createMDX from '@next/mdx'
 
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
   // Other config options
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
 };
 
-export default withNextIntl(nextConfig);
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+})
+
+export default withNextIntl(withMDX(nextConfig));
