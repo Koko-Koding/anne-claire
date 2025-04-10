bc4cc2b855d9dcaba36b771263b1ea968dd0e631
Drikus Roor <drikusroor@gmail.com>
2025-04-10 20:16:55 +0200

    config: Configure base plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,14 @@
+import { EleventyHtmlBasePlugin } from "@11ty/eleventy";
 
 export default function (eleventyConfig) {
+
+  // Add a filter to format urls with base URL
+  eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
 
   eleventyConfig.addPassthroughCopy("public/css/bundle.css");
   
   // pass through all public assets
   eleventyConfig.addPassthroughCopy("public/**/*.*");
-
-
 
   eleventyConfig.setServerOptions({
     liveReload: true,
